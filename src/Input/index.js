654456256b638c2689d81cdda08fbcac8802f74e
npckek <arstarn@yandex.ru>
2023-06-05 20:23:24 +0300

    fisrt commit
    Add input part and change readme file

---
+++ src/Input/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../App.css';
+
+export const Input = () => (
+    <div className='InputWrap'>
+        <input className='Input' />
+        <button className='Button'>+</button>
+    </div>
+);
+
