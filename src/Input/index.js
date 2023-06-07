94bb50832c52145b9c2ac7c037a92eea22bc11b1
npckek <arstarn@yandex.ru>
2023-06-07 17:09:57 +0300

    Update index.js
    Added useState

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../App.css';
 
+const InputTag = () => {
+    const [inputValue, setInputValue] = useState('');
+    const handleOnChange = (event) => {
+        setInputValue(event.target.value);
+    }
+    console.log('render');
+    return (
+        <input className='Input' onChange={handleOnChange} value={inputValue}/>
+    )
+};
+
+const Button = () => (<button className='Button'>+</button>)
+
 export const Input = () => (
-    <div className='InputWrap'>
-        <input className='Input' />
-        <button className='Button'>+</button>
-    </div>
-);
+        <div className='InputWrap'>
+            <InputTag/> 
+            <Button/>
+        </div>
+    )
 
