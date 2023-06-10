4e631819d7a4ce7f5808bb9cee4b21b0a22cf644
npckek <arstarn@yandex.ru>
2023-06-10 16:02:05 +0300

    Added API Key file
    Added API Key file

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,5 +1,6 @@
 import { useState , useEffect } from "react";
 
+import { API_KEY } from '../settings';
 
 
 export const useWeather = (city) => {
---
+++ src/settings.js
@@ -0,0 +1,1 @@
+export const API_KEY =  /* Insert your API key here with in ''*/
