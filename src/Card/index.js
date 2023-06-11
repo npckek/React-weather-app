5bc6d0b191a853e0c5824309261d12331d196b32
npckek <arstarn@yandex.ru>
2023-06-11 18:46:43 +0300

    Update index.js
    Fixed some bugs

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../App.css';
 
-export const Card = ({city}) => (
+import { useWeather } from '../hooks/useWeather';
+
+export const Card = ({city}) => {
+    const data = useWeather(city);
+    if (!data) return null;
+    const { name, weather, main } = data;
+    const { description, icon } = weather[0];
+    const { temp, humidity, feels_like } = main;
+
+    return (
     <div className='Card'>
        <div className='MainInfo'>
-            <img className='Icon' src={`http://openweathermap.org/img/wn/10d@2x.png`} alt='Icon' />
-            <div className='Title'>{city}</div>
-            <div className='Description'>Cloudy</div>
-            <div className='Tempereture'>20</div>
+            <img className='Icon' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='Icon' />
+            <div className='Title'>{name}</div>
+            <div className='Description'>{description}</div>
+            <div className='Tempereture'>{temp.toFixed()}</div>
         </div>
         <div className='Information'>
-            <div>Humidity: 15</div>
-            <div>Feels like:  19</div>
+            <div>Humidity: {humidity}</div>
+            <div>Feels like:  {feels_like}</div>
         </div>
     </div>
-   
-);
+    );
+};
