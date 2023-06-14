facc7d5104ff6868902c38aca9b24f564e34a36e
npckek <arstarn@yandex.ru>
2023-06-14 21:55:58 +0300

    Added globalContext

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import { Input } from './Input';
 
 import { CardList } from './CardList';
 
-import { useCitiesList } from './hooks/useCitiesList'
+import { useCitiesList } from './hooks/useCitiesList';
+
+const GlobalContext = React.createContext();
 
 function App() {
   const [state, dispatch] = useCitiesList();
   const { inputValue, citiesList, editingCity } = state;
   return (
-    <div className="Main">
-      <Input dispatch={dispatch} inputValue={inputValue} editingCity={editingCity} />
-      <CardList citiesList={citiesList} dispatch={dispatch} />
-    </div>
+    <GlobalContext.Provider value={{ state , dispatch }}>
+      <div className="Main">
+        <Input dispatch={dispatch} inputValue={inputValue} editingCity={editingCity} />
+        <CardList citiesList={citiesList} dispatch={dispatch} />
+      </div>
+    </GlobalContext.Provider>
   );
 }
 
