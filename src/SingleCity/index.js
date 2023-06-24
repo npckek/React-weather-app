6bbf3578c283d9350f16feafab459cd2703f5afd
npckek <arstarn@yandex.ru>
2023-06-24 16:20:02 +0300

    Update SingleCity\index.js
    Add some features for the future

--- src/SingleCity/index.js
+++ src/SingleCity/index.js
@@ -2,4 +2,12 @@ import React from "react";
 
 import '../App.css';
 
-export const SingleCity = () => <h1>SingleCity</h1>
+import { Card } from "../Card";
+
+export const SingleCity = (props) => {
+    console.log('props', props);
+    const { city } = props.match.params;
+    return (<Card city={city}/>)
+};
+
+
