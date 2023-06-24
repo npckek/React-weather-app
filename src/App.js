4dcada3bbe7181dbc770c07330c96ef9a255650f
npckek <arstarn@yandex.ru>
2023-06-24 16:19:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 
 import { useCitiesList } from './hooks/useCitiesList';
 
-import { MainPage } from './MainPage';
+import { ErrorBoundary } from './ErrorBoundary';
+
+import { Input } from './Input';
+
+import { CardList } from './CardList';
+
 import { SingleCity } from './SingleCity';
 
 export const GlobalContext = React.createContext();
+
 
 function App() {
   const [state, dispatch] = useCitiesList();
   return (
     <BrowserRouter>
       <GlobalContext.Provider value={{ state, dispatch }}>
-        <Routes>
-          <Route path="/home" element={<MainPage />} />
-          <Route path="/city/:city" element={<SingleCity />} />
-        </Routes>
+        <div className="Main">
+          <Route path="/home">
+            <Input />
+            <ErrorBoundary>
+              <CardList />
+            </ErrorBoundary>
+          </Route>
+          <Route path="/city/:city" render={SingleCity} />
+        </div>;
       </GlobalContext.Provider>
     </BrowserRouter>
   );
