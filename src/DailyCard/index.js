10a4671485a531ce4188f207a339011baf18f441
npckek <arstarn@yandex.ru>
2023-06-27 21:38:17 +0300

    Add Date on DailyCard at SinglePage

--- src/DailyCard/index.js
+++ src/DailyCard/index.js
@@ -7,9 +7,11 @@ export const DailyCard = ({ dailyCard }) => {
     console.log('dailyCard', dailyCard);
     const { dt , weather, main: { temp } } = dailyCard;
     const { main, icon } = weather[0];
+    const currentDate = new Date(dt * 1000);
     return ( 
-
         <div className="DailyCard">
+            <div> { currentDate.toString().split(' ')[0] } { currentDate.toString().split(' ')[2] } </div>
+            <div> { currentDate.toString().split(' ')[4] } </div>
             <img className='Icon' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='Icon' />
             <div className="TemperetureIcon">{ temp.toFixed(0) }</div>
             <div> { main } </div>
